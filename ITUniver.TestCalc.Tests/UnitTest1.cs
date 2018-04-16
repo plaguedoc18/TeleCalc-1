c357a02c1c4b67fc3a3190c9c52d23afd43a8768
eyzmachine <hhjfg@ya.ru>
2018-04-16 12:55:39 +0400

    Added class.

---
+++ ITUniver.TestCalc.Tests/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ITUniver.TeleCalc.ConCalc;
+
+namespace ITUniver.TestCalc.Tests
+{
+    [TestClass]
+    public class CalcTest
+    {
+        [TestMethod]
+        public void TestSum()
+        {
+            //arrange
+            var calc = new Calc();
+            var x = 1;
+            var y = 2;
+
+            //act
+            var result1 = calc.Sum(x, y);
+            var result2 = calc.Sum(10, 0);
+
+            //assert
+            Assert.AreEqual(3, result1);
+            Assert.AreEqual(10, result2);
+        }
+    }
+}
