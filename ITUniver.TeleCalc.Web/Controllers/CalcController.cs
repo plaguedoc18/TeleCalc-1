66c2a424232671db0705fe506f73953ae1bab786
eyzmachine <hhjfg@ya.ru>
2018-04-20 00:37:15 +0400

    Добавил модель калька, вьюху Exec по ней, также сделал список операций.

--- ITUniver.TeleCalc.Web/Controllers/CalcController.cs
+++ ITUniver.TeleCalc.Web/Controllers/CalcController.cs
@@ -1,4 +1,5 @@
 ﻿using ITUniver.TeleCalc.Core;
+using ITUniver.TeleCalc.Web.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,23 +11,51 @@ namespace ITUniver.TeleCalc.Web.Controllers
     public class CalcController : Controller
     {
         [HttpGet]
+        public ActionResult Exec()
+        {
+            Calc action = new Calc();
+            ViewBag.Opers = action.GetOpers;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Exec(CalcModel model)   //Эта модель заполняется
+        {
+            Calc action = new Calc();
+            if (!string.IsNullOrEmpty(model.opername))
+            {
+                ViewBag.OperName = model.opername.ToLower();
+                ViewBag.x = model.X;
+                ViewBag.y = model.Y;
+                model.Result = action.Exec(ViewBag.OperName, ViewBag.x, ViewBag.y);
+            }
+            else
+            {
+                ViewBag.Error = "Что-то пошло не так!";
+            }
+            return View(model);
+        }
+        [HttpGet]
         public ActionResult Index(string opername, double? x, double? y)
         {
             Calc action = new Calc();
-            if (opername != null) ViewBag.OperName = opername.ToLower();
-            ViewBag.x = x;
-            ViewBag.y = y;
-            ViewBag.result = action.Exec(ViewBag.OperName, ViewBag.x, ViewBag.y);
+            if (opername != null && x != null && y != null)
+            {
+                ViewBag.OperName = opername.ToLower();
+                ViewBag.x = x;
+                ViewBag.y = y;
+                ViewBag.result = action.Exec(ViewBag.OperName, ViewBag.x, ViewBag.y);
+            }
+            else
+            {
+                ViewBag.Error = "Что-то пошло не так!";
+            }
             return View();
         }
         public ActionResult Operations()
         {
             Calc ForOpers = new Calc();
-            foreach(string elem in ForOpers.GetOpers)
-            {
-                ViewBag.opers += elem + "\t";
-            }
-            return View();
+            ViewBag.opers = ForOpers.GetOpers;
+            return View("Ops");
         }
     }
 }
---
+++ ITUniver.TeleCalc.Web/Models/CalcModel.cs
@@ -0,0 +1,31 @@
+﻿using ITUniver.TeleCalc.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ITUniver.TeleCalc.Web.Models
+{
+    public class CalcModel
+    {
+        public string[] opers
+        {
+            get
+            {
+                Calc calc = new Calc();
+                return calc.GetOpers;
+            }
+        }
+        [Required(ErrorMessage = "Пропустил!")]
+        public string opername { get; set; }
+        [Required(ErrorMessage = "Пропустил!")]
+        public double X { get; set; }
+        [Required(ErrorMessage = "Пропустил!")]
+        public double Y { get; set; }
+        [ReadOnly(true)]
+        public double Result { get; set; }
+
+    }
+}
